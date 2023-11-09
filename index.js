9a1a3f476d362947d1c31f0961bae00dfb8058af
cerigo-stack <greekhumanist@gmail.com>
2023-11-09 16:57:11 +0200

    lobby retrival added

--- index.js
+++ index.js
@@ -2,28 +2,26 @@ const WebSocketServer = require('ws')
 const Lobbies = require('./lobbies')
 Lobbies.registerLobby("sexking's lobby","sexking")
 Lobbies.registerLobby("deadman's lobby","deadman")
+Lobbies.registerLobby("sexking's lobby","sexking")
+Lobbies.registerLobby("deadman's lobby","deadman")
+Lobbies.registerLobby("sexking's lobby","sexking")
+Lobbies.registerLobby("deadman's lobby","deadman")
 
 const wss = new WebSocketServer.Server({ port: 6336}, () =>
 {
     console.log("Server listening in on port 6336")
 })
 
+
 wss.on('connection', (ws) => 
 {
     ws.on('error', console.error);
-  
     ws.on('message', (data) => 
     {
         switch (data.toString())
         {
             case 'REQ_LOBBIES':
-                console.log(Lobbies.lobby_list.length)
-                for (const lobby of Lobbies.lobby_list) {
-                    console.log("lobby: "+lobby.name)
-                }
-                break
+                ws.send(Lobbies.generate_lobby_message())
         }
-    });
-  
-    ws.send('something');
+    })
 })
--- lobbies.js
+++ lobbies.js
@@ -8,11 +8,20 @@ class lobby
 
 }
 var lobby_list = []
-module.exports.lobby_list = lobby_list
-
 
 module.exports.registerLobby = (name,host) =>
 {
     lobby_list.push(new lobby(name,host))
+}
+
+module.exports.generate_lobby_message = () =>
+{
+    msg="LBS:"
+    for (const lob of lobby_list)
+    {
+        msg+=lob.name+"?"+lob.host+"/"
+    }
+    msg=msg.slice(0,-1) //removes last /
+    return msg
 }
 
